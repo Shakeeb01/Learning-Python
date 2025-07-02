715c03bb985560da698771c3232975cee29f9fb8
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-07-02 07:53:03 +0500

    In this file i learned about different datatypes in python

---
+++ Day_1/data_types.py
@@ -0,0 +1,19 @@
+# ✅ DATA TYPES:
+"""
+As we disscuss earlier that variables store some piece of information/data.
+So that data will have any type for itself.we call that type a data type.
+Python has some built in data types for variables.
+
+
+✅ Type	                                    ✅ Description 	                     ✅ Example
+int	                                        Integer (whole numbers)                    x = 10
+float	                                  Floating-point number (decimal)	          y = 3.14
+complex	                                      Complex numbers	                     z = 2 + 3j
+bool	                                    Boolean (True or False)	                  a = True
+str	String                                         (text)	                       name = "Shakeeb"
+
+
+✅ Checking the Type of data
+---> type(x)
+
+"""
