d0b7cf7befaf693c70b422c0fca80e4e868f8467
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-07-02 07:47:00 +0500

    In this file i learned about comments and variables in python

---
+++ Day_1/comnts_vars.py
@@ -0,0 +1,26 @@
+# ✅ COMMENTS:
+"""
+Comments are something that will always be ignored by the python interpreter.
+There are two types of comments.Single line and Multiline commments.
+Single line comments are done by '#' and multiline is done by three double qoutes.
+"""
+
+
+
+# ✅ VARIABLES:
+"""
+Variables are like a containers to store some piece of information in programing languages.
+We can write anything in variables.
+
+EXAMPLE:
+name = "Shakeeb"
+age = 23
+"""
+
+
+# ✅ VARIABLES DECLARATION RULES:
+"""
+1. We can not use numbers are the start of the variables -->  12shakeeb = "Shakeeb" ❌
+2. We can not use space betweeen variables     --> sha keeb = 'Shakeeb ur Rehman'  ❌
+3. We can no  use special characters.          --> $age = 23 ❌
+"""
